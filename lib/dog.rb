493d3e936b0d5cde44f693ef243cd9119dfe212e
Indu Ramesh <induramesh@gmail.com>
2019-08-30 17:54:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,5 +4,14 @@ class Dog
   
   def initialize(name)
     @name = name
+    @@all.push(name)
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @all.map {|doggo| puts doggo}
   end 
 end 
